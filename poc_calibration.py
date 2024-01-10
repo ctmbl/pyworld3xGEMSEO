87787c83c6f31f6b95396578b523f5f402d9aab1
ctmbl <mabileau.clement@gmail.com>
2024-01-10 10:18:15 +0100

    Separate the custome Calibration discipline to calibration_d.py

--- poc_calibration.py
+++ poc_calibration.py
@@ -1,4 +1,3 @@
-import copy
 import logging
 
 from gemseo import create_design_space, create_scenario, configure_logger
@@ -7,27 +6,7 @@ from gemseo.disciplines.analytic import AnalyticDiscipline
 from gemseo_calibration import calibrator
 import numpy as np
 
-#### Calibration section:
-class Calibration(MDODiscipline):
-    def __init__(self, data, output_name):
-        super().__init__() # init the MDODiscipline parent class
-        self.data_names = list(data.keys())
-        self.data = copy.deepcopy(data)
-        self.output_name = output_name
-        self.n = len(self.data_names)
-        self.d = len(self.data[self.data_names[0]])
-
-        self.input_grammar.update_from_names(self.data_names)
-        self.output_grammar.update_from_names([output_name])
-        
-    def _run(self):
-        A = np.empty((self.n, self.d))
-        for i in range(self.n):
-            data_name = self.data_names[i]
-            A[i] = self.data[data_name] - self.local_data[data_name]
-            logger.debug("A[i]: %s, self.data[data_name] %s", A[i], self.data[data_name])
-        self.local_data[self.output_name] = np.array([np.linalg.norm(A)])
-
+from calibration_d import Calibration
 
 class FakeSimulation(MDODiscipline):
     def __init__(self):
