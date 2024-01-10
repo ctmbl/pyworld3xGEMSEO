e1772e26febf604f69db82fc26cd428deff4d1af
ctmbl <mabileau.clement@gmail.com>
2024-01-10 10:13:46 +0100

    Add comments about the calibration POC: influence of the different parameters

--- poc_calibration.py
+++ poc_calibration.py
@@ -32,21 +32,19 @@ class Calibration(MDODiscipline):
 class FakeSimulation(MDODiscipline):
     def __init__(self):
         super().__init__() # init the MDODiscipline parent class
-        # self.input_grammar.update_from_names(["in11", "in12", "in13", "in21", "in22", "in23"])
-        self.input_grammar.update_from_names(["in11", "in21"])
+
+        self.input_grammar.update_from_names(["in1", "in2"])
         self.output_grammar.update_from_names(["out1", "out2"])
         self.n = 2
         self.d = 2
         
     def _run(self):
-        in11 = self.local_data["in11"][0]
-        # in12 = self.local_data["in12"][0]
-        # in13 = self.local_data["in13"][0]
-        in21 = self.local_data["in21"][0]
-        # in22 = self.local_data["in22"][0]
-        # in23 = self.local_data["in23"][0]
+        in11 = self.local_data["in1"][0]
+        in21 = self.local_data["in2"][0]
+
         self.local_data["out1"] = np.array([in11*x**2 for x in range(self.d)])
         self.local_data["out2"] = np.array([in21*x**2 for x in range(self.d)])
+
         logger.debug("local data out1 : %s", self.local_data["out1"])
         logger.debug("local data out2 : %s", self.local_data["out2"])
 
@@ -61,24 +59,46 @@ fake_sim = FakeSimulation()
 
 
 def main():
+    # 4 orders of magnitude between the two variables "true values"
+    in1_true = 1e4
+    in2_true = -5
+
+    # though we didn't study the influence of the number of points, it might be that
+    # more points allow the obj func to be more precise and thus GEMSEO to opitmize it better
     data = {
-        "out1": np.array([ 5*x**2 for x in range(2)]),
-        "out2": np.array([-5*x**2 for x in range(2)])
+        "out1": np.array([ in1_true * x**2 for x in range(2)]),
+        "out2": np.array([ in2_true * x**2 for x in range(2)])
     }
+    logger.debug("real values: in1_true=%s in2_true=%s", in1_true, in2_true)
     logger.debug("real data: %s", data)
     disc = [fake_sim, Calibration(data, "obj")]
 
     design_space = create_design_space()
-    for key in ["in11", "in12", "in13", "in21", "in22", "in23"]:
-        design_space.add_variable(key, 1, l_b=-10, u_b=10, value=1)
+    for key in ["in1", "in2"]:
+        # large design variables bounds (5 orders of magnitude)
+        design_space.add_variable(key, 1, l_b=-10, u_b=1e5, value=1)
+    """
+    After some experimentations we realized that these three things must be chosen carefully:
+    - design variables true value
+    - design variables bounds
+    - finite difference step dt
+    a too big gap (at leastb 4 orders of magnitude) between these could lead GEMSEO to not find
+    a solution or find a really bad one
+
+    PS: take care to the Optimizer status at the end of a calibration 
+    """
 
     scenario = create_scenario(disc, "MDF", "obj", design_space)
-    scenario.set_differentiation_method("finite_differences", 0.01)
 
-    params = {"max_iter":10, "algo":"SLSQP"}
+    # small dt: bigger or smaller one seems to be worse
+    scenario.set_differentiation_method("finite_differences", 0.001)
+
+    params = {"max_iter":100, "algo":"SLSQP"}
 
     scenario.execute(input_data=params)
 
-logger = configure_logger(level=logging.DEBUG)
+    print(design_space.get_current_value())
+
+logger = configure_logger(level=logging.INFO)
 if __name__ == "__main__":
     main()
