bb1b59ce55c441ffe0533b2827c943621e0218e2
ctmbl <mabileau.clement@gmail.com>
2024-01-15 16:23:33 +0100

    Update our custom Calibration Discipline
    Update parameters name
    Add an exemple of the expected parameter data type/format
    When calcultaing the norm <=> the objective function take into acount
    the fact that each data (coming from databases, there are real world
    historic data) don't necessarily start in 1900 like the simulation. The
    information is given in the data_dict in paramaters of the class
    constructor.²

--- calibration_d.py
+++ calibration_d.py
@@ -5,24 +5,35 @@ from gemseo.core.discipline import MDODiscipline
 import numpy as np
 
 class Calibration(MDODiscipline):
-    def __init__(self, data, output_name):
+    def __init__(self, data_dict, output_name):
         super().__init__() # init the MDODiscipline parent class
         self.logger = logging.getLogger(__name__)
+        """ Example of data format:
+        data = {
+            "fake_p1": {
+                    "offset": 60, # beginning year-1900: 1950 -> 50
+                    "data": np.full((40,), 2e9) # this is an example ofc
+                }, 
+        }
+        """
 
-        self.data_names = list(data.keys())
-        self.data = copy.deepcopy(data)
+        self.data_names = list(data_dict.keys())
+        self.data_dict = data_dict
         self.output_name = output_name
         self.n = len(self.data_names)
-        self.d = len(self.data[self.data_names[0]])
 
         self.input_grammar.update_from_names(self.data_names)
         self.output_grammar.update_from_names([output_name])
 
     def _run(self):
-        A = np.empty((self.n, self.d))
-        for i in range(self.n):
-            data_name = self.data_names[i]
-            A[i] = self.data[data_name] - self.local_data[data_name]
-            self.logger.debug("A[i]: %s, self.data[data_name] %s", A[i], self.data[data_name])
-        self.local_data[self.output_name] = np.array([np.linalg.norm(A)])
+        norms = []
+        for data_name in self.data_names:
+            start = self.data_dict[data_name]["offset"]
+            end = start + len(self.data_dict[data_name]["data"])
+            diff = self.data[data_name]["data"] - self.local_data[data_name][start:end]
+
+            norms.append(np.linalg.norm(diff))
+            self.logger.debug("A[i]=%s, diff=%s", norms[-1], diff)
+        obj = np.linalg.norm(norms)
+        self.local_data[self.output_name] = np.array([obj])
 
